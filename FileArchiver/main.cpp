729b6f2f856e790c55c2fc313df7ff18a12306b8
zhexian <zhexian@zhexian-Parallels>
2015-08-31 09:01:31 +1000

    Created a QtApplication project

---
+++ FileArchiver/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: zhexian
+ *
+ * Created on 29 August 2015, 12:44 AM
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
